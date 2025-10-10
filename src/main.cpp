898d2c2df538d38e44ff945334566e5cfd078bbc
Lidne <george.ermekov@gmail.com>
2025-10-10 19:00:24 +0300

    made lab5 and report

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,101 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-struct Entry {
-    array<uint8_t, 6> key;
-    char value[65];
-};
+std::vector<int> buildSA(const std::string &text) {
+    int n = text.length();
+    std::vector<int> sa(n);
+    for (int i = 0; i < n; i++) {
+        sa[i] = i;
+    }
+    auto suffixComparator = [&text](int a, int b) {
+     return std::lexicographical_compare(text.begin() + a, text.end(), text.begin() + b, text.end()); };
+    std::sort(sa.begin(), sa.end(), suffixComparator);
+    
+    return sa;
+}
+
+// void printSA(const std::vector<int>& sa) {
+//     std::cout << "-------SA-------\n";
+//     int n = sa.size();
+//     for (int i = 0; i < n; i++) {
+//         std::cout << sa[i] << "\n";
+//     }
+//     std::cout << "-------end-------" << std::endl;
+// }
+
+std::vector<int> searchSA(const std::string &text, const std::string &pattern, std::vector<int> &sa) {
+    int patternLength = pattern.size();
+    int arrSize = text.size();
+    int l = 0, r = arrSize - 1;
+    
+    while (l <= r) {
+        int center = l + (r - l) / 2;
+        if (text.substr(sa[center], patternLength) < pattern) {
+            l = center + 1;
+        } 
+        else {
+            r = center - 1;
+        }
+    }
+    
+    int firstOccurrence = l;
+    r = arrSize - 1;
+
+    while (l <= r) {
+        int center = l + (r - l) / 2;
+        if (text.substr(sa[center], patternLength) <= pattern) {
+            l = center + 1;
+        } 
+        else {
+            r = center - 1;
+        }
+    }
+
+    int lastOccurrence = r;
+
+    std::vector<int> resultPositions;
+    for (int idx = firstOccurrence; idx <= lastOccurrence; idx++) {
+        resultPositions.push_back(sa[idx]);
+    }
+
+    return resultPositions;
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-    vector<Entry> entries;
-    string line;
+    std::string text;
+    if (!std::getline(std::cin, text))
+        return 0;
 
-    while (getline(cin, line)) {
-        if (line.empty()) continue;
-        auto tab = line.find('\t');
+    std::vector<int> sa = buildSA(text);
+    // printSA(sa);
 
-        string key_s;
-        key_s.reserve(6);
-        for (char c : line.substr(0, tab)) {
-            if (isalnum(static_cast<unsigned char>(c)))
-                key_s.push_back(c);
+    std::string pattern;
+    int count = 1;
+    while (std::getline(std::cin, pattern))
+    {
+        std::vector<int> foundPositions = searchSA(text, pattern, sa);
+        std::sort(foundPositions.begin(), foundPositions.end());
+        if(foundPositions.empty()) {
+            count++;
+            continue;
         }
-        if (key_s.size() != 6) continue;
-
-        Entry e;
-
-        auto val = line.substr(tab + 1);
-        size_t len = min(val.size(), size_t(64));
-        memcpy(e.value, val.data(), len);
-        e.value[len] = '\0';
-
-        for (int i = 0; i < 6; ++i) {
-            char c = key_s[i];
-            e.key[i] = isalpha(static_cast<unsigned char>(c)) ? c - 'A' : c - '0';
+        int positionsSize = foundPositions.size();
+        std::cout << count << ": ";
+        for (int i = 0; i < positionsSize; i++) {
+            if(i == positionsSize - 1) {
+                std::cout << foundPositions[i] + 1;
+            }
+            else {
+                std::cout << foundPositions[i] + 1 << ", ";
+            }
         }
-        entries.push_back(e);
-    }
-
-    size_t n = entries.size();
-    vector<Entry> buffer(n);
-    array<int, 26> cnt;
-    array<int, 26> pos_arr;
-
-    for (int d = 5; d >= 0; --d) {
-        int range = (d == 0 || d >= 4 ? 26 : 10);
-        cnt.fill(0);
-        for (auto &e : entries) cnt[e.key[d]]++;
-        pos_arr[0] = 0;
-        for (int i = 1; i < range; ++i)
-            pos_arr[i] = pos_arr[i - 1] + cnt[i - 1];
-
-        for (auto &e : entries) {
-            int k = e.key[d];
-            buffer[pos_arr[k]++] = e;
-        }
-        entries.swap(buffer);
-    }
-
-    for (auto &e : entries) {
-        cout << char('A' + e.key[0]) << ' ';
-        cout << int(e.key[1]) << int(e.key[2]) << int(e.key[3]) << ' ';
-        cout << char('A' + e.key[4]) << char('A' + e.key[5]);
-        cout << '\t' << e.value << '\n';
+        std::cout << std::endl;
+        count++;
     }
     return 0;
 }
